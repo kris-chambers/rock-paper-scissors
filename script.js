8cba397885e7de8407e56df9044cef0471696ba9
Kris Chambers <krischambers86@pm.me>
2022-09-27 23:00:03 -0400

    Finish compareSelections() function

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3)
-  let computerChoice
+  let computerSelection
   if (randomNumber === 0)
-    computerChoice = "ROCK"
+    computerSelection = "ROCK"
   else if (randomNumber === 1)
-    computerChoice = "PAPER"
+    computerSelection = "PAPER"
   else if (randomNumber === 2)
-    computerChoice = "SCISSORS"
-  return computerChoice
+    computerSelection = "SCISSORS"
+  return computerSelection
 }
 
-function getPlayerSelection () {
+function getPlayerSelection() {
   playerChoice = prompt("Please choose either rock, paper, or scissors: ")
   let playerSelection = playerChoice.toUpperCase()
   return playerSelection
+}
+
+function compareSelections(playerSelection, computerSelection) {
+  let winner
+  if (playerSelection === computerSelection)
+    winner = tie 
+  else if (playerSelection === "ROCK" && computerSelection === "SCISSORS")
+    winner = player 
+  else if (playerSelection === "PAPER" && computerSelection === "ROCK")
+    winner = player
+  else if (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+    winner = player 
+  else
+    winner = computer 
+}
+
+function declareWinner(winner, playerSelection, computerSelection) {
+  if (winner = player)
+      
+
 }
 
 function playRound(playerSelection, computerSelection) {
